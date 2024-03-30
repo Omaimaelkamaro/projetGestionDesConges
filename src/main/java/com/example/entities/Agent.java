0a53249ffa5e3a05c5280af43d9b071597c4c896
salmaomaima <salmachamam2003@gmail.com>
2024-03-30 22:28:07 +0000

    First Commit

---
+++ src/main/java/com/example/entities/Agent.java
@@ -0,0 +1,18 @@
+package com.example.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+	public class Agent extends User {
+	    // Ajoutez les propriétés spécifiques aux agents si nécessaire
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idAgent;
+	
+	}
+
+
