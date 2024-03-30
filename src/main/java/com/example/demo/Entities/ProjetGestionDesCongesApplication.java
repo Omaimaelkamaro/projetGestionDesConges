72d943afe269b8442fa2cb285c8b9764b6e206a8
Omaimaelkamaro <oelkamaro@gmail.com>
2024-03-30 16:06:42 +0000

    Premier commit

---
+++ src/main/java/com/example/demo/Entities/ProjetGestionDesCongesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.Entities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetGestionDesCongesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetGestionDesCongesApplication.class, args);
+	}
+
+}
